87660b5f0525904c253fba52d208016cf81d071b
insideplant <kami.10pearl@gmail.com>
2021-04-08 15:14:50 +0000

    [Finish]4-4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,15 @@ class FavoritesController < ApplicationController
   
   def create
     post_image = PostImage.find(params[:post_image_id])
-    favolite = current_user.favolites.new(post_image_id: post_image_id)
-    favolite.save
+    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    favorite.save
     redirect_to post_image_path(post_image)
   end
   
   def destroy
     post_image = PostImage.find(params[:post_image_id])
-    favolite = current_user.favolites.find_by(post_image_id: post_image.id)
-    favolite.destroy
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
     redirect_to post_image_path(post_image)
   end
   
