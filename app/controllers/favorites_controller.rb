d69052dc5d466d207b553ca894c47f8e8bc2c78f
insideplant <kami.10pearl@gmail.com>
2021-04-08 14:49:57 +0000

    [start]4-4

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    favolite = current_user.favolites.new(post_image_id: post_image_id)
+    favolite.save
+    redirect_to post_image_path(post_image)
+  end
+  
+  def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    favolite = current_user.favolites.find_by(post_image_id: post_image.id)
+    favolite.destroy
+    redirect_to post_image_path(post_image)
+  end
+  
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c0bb7ac4947afe50b4c1b15bec1b4bba4329d84d848e3efc6c4392c7d31eac7cc3f0c2b4f0ac71aba673c96a5511a5831fac10f4065a5f1a041486049321ecfa'
